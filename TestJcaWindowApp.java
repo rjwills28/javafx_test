7c74542ee7831c6b00d7e92425e96bd4460384c9
Rebecca Williams <rjw@observatorysciences.co.uk>
2024-10-28 10:25:51 +0000

    Add app with JCA updates

---
+++ TestJcaWindowApp.java
@@ -0,0 +1,175 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.application.Platform;
+
+import gov.aps.jca.JCALibrary;
+import gov.aps.jca.Channel;
+import gov.aps.jca.Context;
+import gov.aps.jca.dbr.DBRType;
+import gov.aps.jca.dbr.DBR;
+import gov.aps.jca.dbr.STRING;
+import gov.aps.jca.event.MonitorEvent;
+import gov.aps.jca.event.MonitorListener;
+import gov.aps.jca.Monitor;
+import gov.aps.jca.CAStatus;
+
+public class TestJcaWindowApp extends Application {
+	//Configurable
+	int numWindows = 1;
+	int numWidgets = 20;
+
+	final static int MAX_NUM_WIDGETS = 100;
+
+	public void jcaSetup(Text text, String channelName){
+
+        try {
+        	Context context = null;
+            JCALibrary jca = JCALibrary.getInstance();
+            context = jca.createContext(JCALibrary.CHANNEL_ACCESS_JAVA);
+            Channel channel = context.createChannel(channelName);
+            context.pendIO(1.0);
+
+            Monitor monitor = channel.addMonitor(Monitor.VALUE, new MonitorListenerImpl(text));
+            context.flushIO();
+        } catch (Throwable th) {
+            th.printStackTrace();
+        } 
+    }
+
+	@Override
+	public void start(Stage priStage) throws Exception {
+		int winXpos = 0;
+		int winYpos = 0;
+
+		// Set max number of widget
+		if (numWidgets > MAX_NUM_WIDGETS) {
+			numWidgets = MAX_NUM_WIDGETS;
+		}
+
+		// Create n window
+		for (int n = 0; n < numWindows; n++) {
+			//Adjust position on screen
+			if (n % 5 == 0 && n < 20 && n != 0) {
+				winXpos = 0;
+				winYpos = winYpos + 300;
+			}
+
+			//Create the root node Group object
+			Group group = new Group();
+			ObservableList objsList = group.getChildren();
+
+			// Starting pos
+			int xpos = 50;
+			int ypos = 10;
+			//Create widgets to add to the group
+			for (int i = 0; i < numWidgets; i++) {
+				if (i % 10 == 0) {
+					ypos = ypos + 30;
+					xpos = 50;
+				}
+				//Create a rectangle
+				Rectangle rec = new Rectangle();
+				//Set position from top left
+				rec.setX(xpos);
+				rec.setY(ypos);
+				rec.setWidth(28);
+				rec.setHeight(28);
+				rec.setFill(Color.GREY);
+				rec.setStroke(Color.BLACK);
+				// Add to group
+				objsList.add(rec);
+
+				//Create text
+				Text text = new Text(xpos+5, ypos+18, "");
+				text.setFill(Color.LIME);
+				text.setStrokeWidth(2);
+				objsList.add(text);
+
+				String channelName = "TEST:REC"+i;
+				jcaSetup(text, channelName);
+
+				//Update widget values at 10Hz
+				Task<Void> task = new Task<Void>() {
+					String value = "";
+
+					@Override
+					public Void call() throws Exception{
+						int i = 0;
+						while(true) {
+							if (i == 0) {
+								i = 1;
+								value = "1";
+							} else {
+								i = 0;
+								value = "0";
+							}
+							Platform.runLater(() -> {
+								text.setText(value);
+							});
+							Thread.sleep(100);
+						}
+					}
+				};
+				Thread th = new Thread(task);
+				//th.setDaemon(true);
+				//th.start();
+
+				// Adjust xpos of next widget
+				xpos = xpos + 30;
+			}
+
+			//Create a scene of height and width
+			Scene scene = new Scene(group, 400, 400);
+
+			//Give it a colour
+			scene.setFill(Color.GREY);
+
+			// Create a new Stage (window)
+			Stage secondStage = new Stage();
+			secondStage.setTitle("Test Standalone Window " + (n + 1));
+			secondStage.setWidth(400);
+			secondStage.setHeight(400);
+			secondStage.setX(winXpos);
+			secondStage.setY(winYpos);
+			secondStage.setScene(scene);
+			secondStage.show();
+
+			winXpos = winXpos + 350;
+		}
+	}
+
+	private static class MonitorListenerImpl implements MonitorListener {
+		private Text text;
+
+		MonitorListenerImpl(Text text) {
+			this.text = text;
+		}
+        public void monitorChanged(MonitorEvent event) {
+            // immediately print info
+            DBR dbr = null;
+            if (event.getStatus() == CAStatus.NORMAL) {
+                dbr = event.getDBR();
+                double[] val = ((double[] ) dbr.getValue());
+                Platform.runLater(() -> {
+					text.setText(String.valueOf(val[0]));
+				});
+                //System.out.println(val[0]);
+            } else
+                System.err.println("Monitor error: " + event.getStatus());
+        }
+    }
+
+	
+
+	public static void main(String args[]) {
+
+		launch(args);
+	}
+}
